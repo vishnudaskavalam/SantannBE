3594d63d9318d37e902122b73a3d5bfde6c32f80
vishnu <vishnu62696@gmail.com>
2026-04-15 17:04:50 +0530

    intitial commit

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
